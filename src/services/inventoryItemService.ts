cfd4cada8b710b915d44d2317fb2462901e8102e
Levan Ilashvili <levanilashvili2005@gmail.com>
2026-03-12 17:27:13 +0400

    change api url

--- src/services/inventoryItemService.ts
+++ src/services/inventoryItemService.ts
@@ -1,17 +1,17 @@
 import { api } from "./api";
 
 export const getInventoryItems = async (inventoryId: string) => {
-  const res = await api.get(`/api/inventory/${inventoryId}/items`);
+  const res = await api.get(`/inventory/${inventoryId}/items`);
   return res.data;
 };
 
 export const addItem = async (data: any) => {
-  const res = await api.post(`/api/inventoryitem`, data);
+  const res = await api.post(`/inventoryitem`, data);
   return res.data;
 };
 
 export const updateItem = async (itemId: string, fields: any[]) =>
-  api.put(`/api/inventoryitem/${itemId}`, fields);
+  api.put(`/inventoryitem/${itemId}`, fields);
 
 export const deleteItems = async (ids: string[]) =>
-  api.delete(`/api/inventoryitem`, { data: ids });
+  api.delete(`/inventoryitem`, { data: ids });
--- src/services/inventoryService.ts
+++ src/services/inventoryService.ts
@@ -1,15 +1,13 @@
 import { type Inventory } from "../types/inventory";
 import { api } from "./api";
 
-const API = "https://localhost:7117/api";
-
 export const getInventory = async (id: string) => {
-  const res = await api.get(`${API}/inventory/${id}`);
+  const res = await api.get(`/inventory/${id}`);
   return res.data;
 };
 
 export const getUserInventories = async () => {
-  const res = await api.get<Inventory[]>(`${API}/user/inventories`);
+  const res = await api.get<Inventory[]>(`/user/inventories`);
   return res.data;
 };
 
@@ -19,16 +17,16 @@ export const createInventory = async (data: {
   categoryId: string;
   isPublic: boolean;
 }) => {
-  const res = await api.post(`${API}/inventory`, data);
+  const res = await api.post(`/inventory`, data);
   return res.data;
 };
 
 export const updateInventory = async (id: string, data: any) => {
-  await api.put(`${API}/inventory/${id}`, data);
+  await api.put(`/inventory/${id}`, data);
 };
 
 export const deleteInventory = async (ids: string[]) => {
-  await api.delete(`${API}/inventory`, {
+  await api.delete(`/inventory`, {
     data: ids,
   });
 };
